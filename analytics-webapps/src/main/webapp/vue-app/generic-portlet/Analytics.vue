d8eacba7462ebd29e23a7ce19077cce7ed9a22e8
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-12-03 10:44:04 +0100

    Truncate Title of chart for Mobile view

--- analytics-webapps/src/main/webapp/vue-app/generic-portlet/Analytics.vue
+++ analytics-webapps/src/main/webapp/vue-app/generic-portlet/Analytics.vue
@@ -50,7 +50,7 @@
           </v-tooltip>
           <div
             :title="title"
-            class="my-auto">
+            class="my-auto text-truncate">
             {{ title }}
           </div>
         </v-toolbar-title>
