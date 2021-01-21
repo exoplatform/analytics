575d0691cfa507adb20af690b1745beff225f296
mohamed <mrdidi@exoplatform.com>
2021-01-21 09:10:29 +0100

    clean code

--- analytics-listeners/src/main/java/org/exoplatform/analytics/listener/social/AnalyticsProfileListener.java
+++ analytics-listeners/src/main/java/org/exoplatform/analytics/listener/social/AnalyticsProfileListener.java
@@ -3,7 +3,6 @@ package org.exoplatform.analytics.listener.social;
 import static org.exoplatform.analytics.utils.AnalyticsUtils.*;
 
 import org.exoplatform.analytics.model.StatisticData;
-import org.exoplatform.social.core.identity.model.Profile;
 import org.exoplatform.social.core.model.AvatarAttachment;
 import org.exoplatform.social.core.profile.ProfileLifeCycleEvent;
 import org.exoplatform.social.core.profile.ProfileListenerPlugin;
@@ -12,8 +11,13 @@ public class AnalyticsProfileListener extends ProfileListenerPlugin {
 
   @Override
   public void avatarUpdated(ProfileLifeCycleEvent event) {
-    StatisticData statisticData = addProfileStatisticEvent("avatar", event);
-    addStatisticData(statisticData);
+    AvatarAttachment avatarAttachment = ((AvatarAttachment) event.getPayload().getProperty(AVATAR));
+    if (avatarAttachment != null){
+      StatisticData statisticData = buildStatisticData("avatar", event.getSource());
+      statisticData.addParameter(IMAGE_SIZE, avatarAttachment.getSize());
+      statisticData.addParameter(IMAGE_TYPE, avatarAttachment.getMimeType());
+      addStatisticData(statisticData);
+    }
   }
 
   @Override
@@ -49,23 +53,6 @@ public class AnalyticsProfileListener extends ProfileListenerPlugin {
     long currentUserIdentityId = getCurrentUserIdentityId();
     if (currentUserIdentityId > 0) {
       statisticData.addParameter(FIELD_MODIFIER_USER_SOCIAL_ID, currentUserIdentityId);
-    }
-    return statisticData;
-  }
-  private StatisticData addProfileStatisticEvent(String operation, ProfileLifeCycleEvent event){
-    StatisticData statisticData = new StatisticData();
-    String username = event.getSource();
-    statisticData.setModule("social");
-    statisticData.setSubModule("profile");
-    statisticData.setOperation(operation);
-    statisticData.setUserId(getUserIdentityId(username));
-    long currentUserIdentityId = getCurrentUserIdentityId();
-    AvatarAttachment avatarAttachment = ((AvatarAttachment) event.getPayload().getProperty(AVATAR));
-    if (currentUserIdentityId > 0) {
-      statisticData.addParameter(FIELD_MODIFIER_USER_SOCIAL_ID, currentUserIdentityId);
-    }else if (avatarAttachment != null){
-      statisticData.addParameter(IMAGE_SIZE, avatarAttachment.getSize());
-      statisticData.addParameter(IMAGE_TYPE, avatarAttachment.getMimeType());
     }
     return statisticData;
   }
