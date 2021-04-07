adbfdf77791e3b15feed542479889db9a8b83bc6
mohamed <mrdidi@exoplatform.com>
2021-04-07 11:26:58 +0100

    Change the UI of the filter field

--- analytics-webapps/src/main/webapp/vue-app/table-portlet/components/AnalyticsTableApplication.vue
+++ analytics-webapps/src/main/webapp/vue-app/table-portlet/components/AnalyticsTableApplication.vue
@@ -16,6 +16,7 @@
         :labels="suggesterLabels"
         :include-users="useUsersInSearch"
         :include-spaces="useSpacesInSearch"
+        :inner-icon="true"
         name="selectedUser"
         class="analytics-table-suggester mr-2" />
       <template v-if="canEdit">
