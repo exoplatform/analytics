e96fd13d80d8dba919a6b75ee10f67e78cc6cac3
Mohamed El Hedi Rdid <54801286+Rdidmohamedelhadi@users.noreply.github.com>
2021-04-09 09:16:46 +0100

    Task 42671 : Change the UI of the filter field (#99) (#102)

--- analytics-webapps/src/main/webapp/vue-app/table-portlet/components/AnalyticsTableApplication.vue
+++ analytics-webapps/src/main/webapp/vue-app/table-portlet/components/AnalyticsTableApplication.vue
@@ -16,6 +16,7 @@
         :labels="suggesterLabels"
         :include-users="useUsersInSearch"
         :include-spaces="useSpacesInSearch"
+        filter-style
         name="selectedUser"
         class="analytics-table-suggester mr-2" />
       <template v-if="canEdit">
