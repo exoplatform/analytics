5f4b5059ff338a363a94de580a833452a9394802
Romain Dénarié <romain.denarie@exoplatform.com>
2021-11-17 16:46:43 +0100

    Fix sonar feedbacks'

--- analytics-listeners/src/main/java/org/exoplatform/analytics/listener/translation/AutomaticTranslationListener.java
+++ analytics-listeners/src/main/java/org/exoplatform/analytics/listener/translation/AutomaticTranslationListener.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2021 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.exoplatform.analytics.listener.translation;
 
 import org.exoplatform.analytics.model.StatisticData;
@@ -7,9 +23,6 @@ import org.exoplatform.services.listener.Event;
 import org.exoplatform.services.listener.Listener;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
-import org.exoplatform.social.core.space.model.Space;
-
-import static org.exoplatform.analytics.utils.AnalyticsUtils.addSpaceStatistics;
 
 public class AutomaticTranslationListener  extends Listener<String,AutomaticTranslationEvent> {
 
