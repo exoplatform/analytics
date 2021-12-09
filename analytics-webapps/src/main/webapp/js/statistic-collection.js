63ca4d6a0445e25bfb24a2ec1d8b6a519026510a
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2021-12-09 17:00:56 +0100

    Task-52129: Analytics to know the use of favorites for activities (#209)
    Add statistic each time a user choose to add a favorite to an activity

--- analytics-webapps/src/main/webapp/js/statistic-collection.js
+++ analytics-webapps/src/main/webapp/js/statistic-collection.js
@@ -36,6 +36,7 @@ function() {
         // First time init, install listener
         document.addEventListener('exo-statistic-message', event => this.sendMessage(event && event.detail));
         document.addEventListener('search-connector-selected', event => this.addStatisticSearchFilter(event && event.detail));
+        document.addEventListener('favorite-added', event => this.addStatisticFavorite(event && event.detail));
         document.addEventListener('search-favorites-selected', () => this.sendMessage(
             {
               'module': 'portal',
@@ -86,6 +87,40 @@ function() {
         'timestamp': Date.now()
       };
       this.sendMessage(connectorAnalytics);
+    },
+
+    addStatisticFavorite: function (eventDetail) {
+      let favorite;
+      if (eventDetail.templateParams.newsId) {
+        favorite = {
+          'module': 'portal',
+          'subModule': 'ui',
+          'userId': eXo.env.portal.userIdentityId,
+          'userName': eXo.env.portal.userName,
+          'parameters': {
+            'type': 'News',
+            'contentId': eventDetail.templateParams.newsId,
+            'spaceId': eventDetail.spaceId ? eventDetail.spaceId : eventDetail.templateParams.spaceId,
+          },
+          'operation': 'Bookmark',
+          'timestamp': Date.now()
+        };
+      } else {
+        favorite = {
+          'module': 'portal',
+          'subModule': 'ui',
+          'userId': eXo.env.portal.userIdentityId,
+          'userName': eXo.env.portal.userName,
+          'parameters': {
+            'type': eventDetail.type,
+            'activityId': eventDetail.id,
+            'spaceId': eventDetail.spaceId,
+          },
+          'operation': 'Bookmark',
+          'timestamp': Date.now()
+        };
+      }
+      this.sendMessage(favorite);
     },
 
     installWatchers: function () {
